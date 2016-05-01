c144ee55b8fd225dfd9e8dee25108f635ca64feb
Kuldeep Singh <kuldeepsinghk.com>
2016-05-01 12:30:50 +0530

    add test

---
+++ test/intro_ai/depth_first_serach_test.clj
@@ -0,0 +1,9 @@
+(ns intro-ai.depth-first-serach-test
+  (:require [clojure.test :refer :all]
+            [intro-ai.depth_first_serach :refer :all]))
+
+
+
+(deftest depth_first_serach-test
+  (is (= ' (findpath  stack) ))
+  )
